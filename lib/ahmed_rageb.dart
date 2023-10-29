8d8e60b1b90c3b388ceeea010a05379b6562361b
ahmed ragab <ar1755@fayoum.edu.eg>
2023-10-29 03:45:01 +0200

    ..

--- lib/ahmed_rageb.dart
+++ lib/ahmed_rageb.dart
@@ -17,7 +17,6 @@ class AppSliderAndCategory extends StatelessWidget {
         designSize: const Size(360, 690),
         minTextAdapt: true,
         splitScreenMode: true,
-        // Use builder only if you need to use library outside ScreenUtilInit context
         builder: (_, child) {
           return MaterialApp(
             initialRoute: HomeCategory.routName,
@@ -37,85 +36,80 @@ class HomeCategory extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.only(left: 8.0.w),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  child: CarouselSlider(
-                    items: const [
-                      SizedBox(
-                        child: BuildImage(
-                          image: 'assets/images/property1_default.png',
-                          index: 0,
-                        ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: CarouselSlider(
+                  items: const [
+                    SizedBox(
+                      child: BuildImage(
+                        image: 'assets/images/property1_default.png',
+                        index: 0,
                       ),
-                      SizedBox(
-                        child: BuildImage(
-                          image: 'assets/images/group12.png',
-                          index: 1,
-                        ),
+                    ),
+                    SizedBox(
+                      child: BuildImage(
+                        image: 'assets/images/group12.png',
+                        index: 1,
                       ),
-                      SizedBox(
-                        child: BuildImage(
-                          image: 'assets/images/group13.png',
-                          index: 2,
-                        ),
-                      )
-                    ],
-                    options: CarouselOptions(
-                      autoPlay: true,
-                      aspectRatio: 2,
-                      viewportFraction: 1,
-                      enableInfiniteScroll: true,
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Categories',
-                      style: GoogleFonts.poppins(
-                          fontSize: 18.sp, fontWeight: FontWeight.w500),
-                    ),
-                    Text(
-                      'view all',
-                      style: GoogleFonts.poppins(
-                          fontSize: 12.sp, fontWeight: FontWeight.w400),
-                    ),
+                    SizedBox(
+                      child: BuildImage(
+                        image: 'assets/images/group13.png',
+                        index: 2,
+                      ),
+                    )
                   ],
-                ),
-                SizedBox(
-                  height: 24.h,
-                ),
-                SizedBox(
-                  height: 450.h,
-                  child: GridView.builder(
-                    scrollDirection: Axis.horizontal,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 0,
-                      childAspectRatio:
-                          2, // Aspect ratio of the items (1 for square)
-                    ),
-                    itemCount: 80, // Total number of items
-                    itemBuilder: (BuildContext context, int index) {
-                      return BuildCategory(
-                        img:
-                            'https://th.bing.com/th/id/R.16e5970f938011ce262701a9e2cc2dd1?rik=ffV0C3V0MCn45g&pid=ImgRaw&r=0',
-                        description: 'Error 404',
-                      );
-                    },
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    aspectRatio: 2,
+                    viewportFraction: 1,
+                    enableInfiniteScroll: true,
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: GoogleFonts.poppins(
+                        fontSize: 18.sp, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'view all',
+                    style: GoogleFonts.poppins(
+                        fontSize: 12.sp, fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24.h,
+              ),
+              Expanded(
+                child: GridView.builder(
+                  scrollDirection: Axis.horizontal,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    mainAxisSpacing: 3,
+                    crossAxisCount: 2,
+                    childAspectRatio: 2.3,
+                  ),
+                  itemCount: 80,
+                  itemBuilder: (BuildContext context, int index) {
+                    return const BuildCategory(
+                      img:
+                          'https://th.bing.com/th/id/R.16e5970f938011ce262701a9e2cc2dd1?rik=ffV0C3V0MCn45g&pid=ImgRaw&r=0',
+                      description: 'Error 404',
+                    );
+                  },
+                ),
+              )
+            ],
           ),
         ),
       ),
@@ -186,7 +180,9 @@ class BuildCategory extends StatelessWidget {
           child: Text(
             description,
             style: GoogleFonts.poppins(
-                fontSize: 14.sp, fontWeight: FontWeight.w400),
+                color: const Color(0xff06004F),
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w400),
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
           ),
